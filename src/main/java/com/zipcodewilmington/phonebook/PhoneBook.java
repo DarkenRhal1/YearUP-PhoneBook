3273fcd229c43f82845f86ea133aa877b4c0f976
Yaw <baffourampadu06@gmail.com>
2023-10-13 15:15:28 -0400

    Add all changes

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -13,42 +10,75 @@ import java.util.Map;
  */
 public class PhoneBook {
 
-    private final Map<String, List<String>> phonebook;
+    private Map<String, List<String>> phonebook;
 
-    public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+    public PhoneBook(Map<String, List<String>> phonebook) {
+        this.phonebook = new LinkedHashMap<>(phonebook);
     }
 
-    public PhoneBook() {
-        this(null);
+    public PhoneBook(){
+        this.phonebook = new LinkedHashMap<>();
     }
 
-    public void add(String name, String phoneNumber) {
+    public void add(String name, String phoneNumber){
+        if (phonebook.containsKey(name)) {
+            List <String> numbers = phonebook.get(name);
+            numbers.add(phoneNumber);
+
+        }
+        List<String> phoneNumbers = new ArrayList<>();
+        phoneNumbers.add(phoneNumber);
+        phonebook.put(name, phoneNumbers);
+
+
     }
 
-    public void addAll(String name, String... phoneNumbers) {
+    public void addAll(String name, String[] phoneNumbers){
+        if (phonebook.containsKey(name)) {
+            List <String> numbers1 = phonebook.get(name);
+            numbers1.add(phoneNumbers[0]);
+            numbers1.add(phoneNumbers[1]);
+        }
+        List <String> phoneNumbers1 = new ArrayList<>(Arrays.asList(phoneNumbers));
+        phonebook.put(name, phoneNumbers1);
+//        if (!phonebook.containsKey(phoneNumbers)){
+//            phonebook.put(name, new ArrayList<>());
+//        }
+//
+//        for (int i = 0; i < phoneNumbers.length; i++) {
+//            phonebook.put(name, new ArrayList<>()).add(Arrays.toString(phoneNumbers));
+//        }
+
     }
 
-    public void remove(String name) {
+    public void remove(String name){
+        phonebook.remove(name);
     }
 
-    public Boolean hasEntry(String name) {
+    public boolean hasEntry(String name){
+        return  phonebook.containsKey(name);
+    }
+
+    public List<String> lookup(String name){
+        return phonebook.get(name);
+    }
+
+    public String reverseLookup(String phoneNumber) {
+        for (Map.Entry<String, List<String> > entry : phonebook.entrySet()){
+            if (entry.getValue().contains(phoneNumber)) {
+                return entry.getKey();
+            }
+        }
         return null;
     }
 
-    public List<String> lookup(String name) {
-        return null;
-    }
-
-    public String reverseLookup(String phoneNumber)  {
-        return null;
-    }
-
-    public List<String> getAllContactNames() {
-        return null;
+    public List<String> getAllContactNames(){
+        List<String> nameList = new ArrayList<>();
+        nameList.addAll(phonebook.keySet());
+        return nameList;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
